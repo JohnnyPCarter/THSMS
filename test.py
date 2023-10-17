fcfd6ff2595c69048a0493d5db96ce5909684d2d
Johnny <carte101@miamioh.edu>
2023-10-17 13:35:11 -0400

    ready for test 1

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ count = 0
 frameCount = 0
 fileNumber = 0
 countdown = 0
-DEFAULT_FILE_NAME = "/usr/local/var/www/event"
+DEFAULT_FILE_NAME = "/var/www/event"
 
 # Define the codec and create VideoWriter object
 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
