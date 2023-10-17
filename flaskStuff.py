a4da284edcdecd1000cf1ba2a5484e4f1e50db7a
Johnny <carte101@miamioh.edu>
2023-10-16 20:53:23 -0400

    Now it has flask ;)

---
+++ flaskStuff.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+database = "MediaCenter.db"
+
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM files ORDER BY CreatedDate DESC LIMIT 10;')
+    data = cursor.fetchall()
+    conn.close()
+    return jsonify(data)
+
+@app.route('/add_data', methods=['POST'])
+def add_data():
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    data = request.get_json()
+    cursor.execute('INSERT INTO files (FileName, Region, CreatedDate) VALUES (?, ?, ?);', (data['FileName'], data['Region'], data['CreatedDate']))
+    conn.commit()
+    conn.close()
+    return 'Data added successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)
